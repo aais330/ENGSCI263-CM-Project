22222e2dc7882b8d77450aff9438fc9f77054541
Anton Aish <antonaish@Antons-MacBook-Pro.local>
2020-08-26 18:00:29 +1200

    Reformated the modified derivative script and added a few benchmarks

--- Modified_Derivates.py
+++ Modified_Derivates.py
@@ -33,6 +33,147 @@ def improved_euler_step(f, tk, xk, h, pars = []):
     return xk + (h/2)*(f(xk,tk,*pars) + f(xk1_prediction, tk+h, *pars))
 
 
+# Pressure ODE
+def dPdt(P, t, b):
+    '''
+    Parameters:
+    -----------
+    P : float
+        pressure value (dependent variable)
+    t : float
+        time value (independent variable)
+    b : float
+        Recharge strength parameter 
+        
+    Returns:
+    -------
+     dPdt : float
+        rate of change of pressure in aquifer
+    '''
+    dP_a = 0.5 # Pressure difference across aquifer
+    t_mar = 2020 # Time when MAR begins
+
+    if (t>t_mar):
+        dP_a += 0.5 # Pressure difference increase due to MAR
+        
+    return -b*(P + dP_a/2) -b*(P-(dP_a)/2) 
+
+# Concentration ODE
+def dCdt(ci, t, P, b1, alpha, bc, tau):
+    '''
+    Parameters
+    ----------
+    ci : float
+        Concentration of nitrate (dependent variable)
+    t : float
+        time value (independent variable)
+    P : float
+        Current pressure of aquifer
+    b1 : float
+        infliltration parameter
+    alpha : float
+        Active carbon sink infiltration modication parameter
+    bc : float
+        dilution parameter
+    tau : float
+        time lag paramter
+
+    Returns
+    -------
+    dCdt : float
+        rate of change of concentration in aquifer
+    '''
+    dP_a = 0.1 # Pressure difference across aquifer
+    dP_surf = 0.05 # Oversurface pressure
+    t_mar = 2020 # Time when MAR begins
+    t_acs = 2020 # Time active carbon sink was installed
+
+
+    # number of cows
+    tn, n = np.genfromtxt('nl_cows.txt', delimiter=',', skip_header=1).T
+    
+    if ((t-tau) > 1990.5):
+        ni = np.interp(t-tau,tn,n) #interpolating number of cows
+    else:
+        ni = n[0]
+    
+
+    # Active carbon sink
+    if (t>t_acs):
+        b1 = alpha*b1
+
+    # MAR
+    if (t>t_mar):
+        dP_a += 0.5 # Pressure difference increase due to MAR
+             
+    return  -ni*b1*(P-dP_surf)+bc*ci*(P-(dP_a/2))
+
+
+def solve_dPdt(f, t, pi, b):
+    '''
+    Parameters:
+    -----------
+    f : callable
+        Function that returns dxdt given variable and parameter inputs.
+    t : array-like
+        array of floats containing time value.
+    P0 : float
+        Initial value of solution.
+     b : float
+        recharge strength parameter
+        
+    Returns: 
+    --------
+    P : array like
+        array of floats containing pressure in aquifer
+    '''
+    P = np.zeros(t.shape) # intialising pressure vector
+    P[0] = pi 
+    dt = t[1] - t[0]
+
+    # Solve using Improved Euler Method
+    for i in range(len(t)-1):
+        P[i+1] = improved_euler_step(f,t[i],P[i],dt,b)
+        
+    return P
+
+# Solve concentration ODE
+def solve_dCdt(f,t,P, b1, alpha, bc, tau):
+    '''
+    Parameters:
+    -----------
+    f : callable
+        function that returns dxdt given variable and parameter inputs.
+    t : array-like
+        array of floats containing time value.
+    P : array-like
+        array of floats containing the pressure within the aquifer
+    b1 : float
+        infliltration parameter
+    alpha : float
+        Active carbon sink infiltration modication parameter
+    bc : float
+        dilution parameter
+    tau : float
+        time lag paramter
+
+    Returns:
+    -------
+    C : array-like
+        An array of concentrations
+    '''
+    C = np.zeros(t.shape) # intialising concentration array
+    C[0] = 0.2
+    dt = t[1]-t[0]
+
+    # Solve using improved euler method
+    for i in range(len(t)-1):
+        pars = [P[i],b1,alpha,bc,tau]
+        output = improved_euler_step(f,t[i],C[i],dt,pars)
+        C[i+1] = output
+
+    return C
+
 
 '''
 LPM_Model is a single function that solves the LPM for nitrate concentration in the aquifer
@@ -69,161 +210,14 @@ def LMP_Model(t,pi, b ,b1, alpha, bc, tau):
     # load in cow data and concentration data
     tn, n = np.genfromtxt('nl_cows.txt', delimiter=',', skip_header=1).T
     tcon, c = np.genfromtxt('nl_n.csv', delimiter=',', skip_header=1).T
-
-    #Pressure ODE
-    def dPdt(P, t, b):
-        '''
-        Parameters:
-        -----------
-        P : float
-            pressure value (dependent variable)
-        t : float
-            time value (independent variable)
-        b : float
-            Recharge strength parameter 
-        
-        Returns:
-        -------
-        dPdt : float
-            rate of change of pressure in aquifer
-        '''
-        dP_a = 0.5 # Pressure difference across aquifer
-        t_mar = 2020 # Time when MAR begins
-
-        if (t>t_mar):
-            dP_a += 0.5 # Pressure difference increase due to MAR
-        
-        dPdt = -b*(P + dP_a/2) -b*(P-(dP_a)/2) 
-        return dPdt
-
-   
-    def solve_dPdt(f, t, pi, b):
-        '''
-        Parameters:
-        -----------
-        f : callable
-            Function that returns dxdt given variable and parameter inputs.
-        t : array-like
-            array of floats containing time value.
-        P0 : float
-            Initial value of solution.
-        b : float
-            recharge strength parameter
-        
-        Returns: 
-        --------
-        P : array like
-            array of floats containing pressure in aquifer
-        '''
-        P = np.zeros(t.shape) # intialising pressure vector
-        P[0] = pi 
-        dt = t[1] - t[0]
-
-        # Solve using Improved Euler Method
-        for i in range(len(t)-1):
-            P[i+1] = improved_euler_step(f,t[i],P[i],dt,b)
-        
-        return P
-
+      
     # Solve pressure ODE
     P = solve_dPdt(dPdt,t,pi,[b])
-    
-    # Concentration ODE
-    def dCdt(ci, t, P, b1, alpha, bc, tau):
-        '''
-        Parameters
-        ----------
-        ci : float
-            Concentration of nitrate (dependent variable)
-        t : float
-            time value (independent variable)
-        P : float
-            Current pressure of aquifer
-        b1 : float
-            infliltration parameter
-        alpha : float
-            Active carbon sink infiltration modication parameter
-        bc : float
-            dilution parameter
-        tau : float
-            time lag paramter
 
-        Returns
-        -------
-        dCdt : float
-            rate of change of concentration in aquifer
-        '''
-        dP_a = 0.1 # Pressure difference across aquifer
-        dP_surf = 0.05 # Oversurface pressure
-        t_mar = 2020 # Time when MAR begins
-        t_acs = 2020 # Time active carbon sink was installed
-
-
-        # number of cows
-        tn, n = np.genfromtxt('nl_cows.txt', delimiter=',', skip_header=1).T
-        '''
-        if ((t-tau) > 1990.5):
-            ni = np.interp(t-tau,tn,n) #interpolating number of cows
-        else:
-            ni = n[0]
-        '''
-        ni = 0
-
-        # Active carbon sink
-        if (t>t_acs):
-            b1 = alpha*b1
-
-        # MAR
-        if (t>t_mar):
-            dP_a += 0.5 # Pressure difference increase due to MAR
-
-        change = -ni*b1*(P-dP_surf)+bc*ci*(P-(dP_a/2))
-        return  change
-
-    # Solve concentration ODE
-    def solve_dCdt(f,t,P, b1, alpha, bc, tau):
-        '''
-        Parameters:
-        -----------
-        f : callable
-            Function that returns dxdt given variable and parameter inputs.
-        t : array-like
-            array of floats containing time value.
-        P : array-like
-            array of floats containing the pressure within the aquifer
-        b1 : float
-            infliltration parameter
-        alpha : float
-            Active carbon sink infiltration modication parameter
-        bc : float
-            dilution parameter
-        tau : float
-            time lag paramter
-
-        Returns:
-        -------
-        C : array-like
-            An array of concentrations
-        '''
-        C = np.zeros(t.shape) # intialising concentration array
-        C[0] = 0.2
-        dt = t[1]-t[0]
-
-        # Solve using improved euler method
-        for i in range(len(t)-1):
-            pars = [P[i],b1,alpha,bc,tau]
-            output = improved_euler_step(f,t[i],C[i],dt,pars)
-            C[i+1] = output
-
-        return C
-
-    # Solve concentration ODE
+    # Solve concentration ODE 
     C = solve_dCdt(dCdt,t,P,b1,alpha,bc,tau)
-    plt.plot(t,C)
-    plt.show()
 
-
-    return 0
+    return C
 
 
 # Solve pressure ODE
---
+++ Modified_Derivates_Benchmark.py
@@ -0,0 +1,85 @@
+# This script benchmarks the two ODEs formulated in the Modified_Derivates script
+
+from Modified_Derivates import *
+
+# Pressure benchmark without MAR
+if False:
+    # Analytical Solution
+    t = np.arange(1980,2020,0.5)
+    P_analytical = np.zeros(t.shape)
+
+    # Numerical Solution
+    P_numerical = solve_dPdt(dPdt,t,0,[1])
+
+    f,ax = plt.subplots(1,1)
+
+    ax.plot(t,P_analytical,'b', label = 'Analytical Solution')
+    ax.plot(t,P_numerical,'r+', label = 'Numeric Solution')
+
+    ax.set_title('Analytical vs Numeric solution')
+    plt.ylabel('Pressure')
+    plt.xlabel('Time')
+    plt.xlim(1980,2020)
+    plt.ylim(-5,5)
+
+    plt.show()
+
+# Concentration benchmark with no cows
+# NOTE: to run this test you must go into the concentration derivative function (dCdt) and 
+# set the number of cows to 0.
+if False:    
+    t = np.arange(1980,2020,0.5)
+    C_Analytical = np.exp((-0.05*t + 99))*0.2
+
+    b=0.5
+    b1=0.5
+    alpha=0
+    bc=1
+    tau = 5
+    
+    P_numerical = solve_dPdt(dPdt,t,0,[1])
+    C_Numerical = solve_dCdt(dCdt,t,P_numerical,b1,alpha,bc,tau)
+
+    f,ax = plt.subplots(1,1)
+
+    ax.plot(t,C_Analytical,'b', label = 'Analytical Solution')
+    ax.plot(t,C_Numerical,'r+', label = 'Numeric Solution')
+
+    ax.set_title('Analytical vs Numeric solution (no cows)')
+    plt.ylabel('Concentration')
+    plt.xlabel('Time')
+    
+    plt.show()
+
+# Constant number of cows (100)
+# NOTE: to run this test you must go into the concentration derivative function (dCdt) and 
+# set the number of cows to 100
+if False:
+    t = np.arange(1980,2020,0.5)
+    C_Analytical = (-499/5)*np.exp((-t+1980)/20) + 100 
+
+    b=1
+    b1=1
+    alpha=0
+    bc=1
+    tau = 0
+    
+    P_numerical = solve_dPdt(dPdt,t,0,[1])
+    C_Numerical = solve_dCdt(dCdt,t,P_numerical,b1,alpha,bc,tau)
+
+    f,ax = plt.subplots(1,1)
+
+    ax.plot(t,C_Analytical,'b', label = 'Analytical Solution')
+    ax.plot(t,C_Numerical,'r+', label = 'Numeric Solution')
+
+    ax.set_title('Analytical vs Numeric solution (100 cows)')
+    plt.ylabel('Concentration')
+    plt.xlabel('Time')
+    
+    plt.show()
+
+
+
+
+
+
