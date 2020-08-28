88b6a1390e05023d9c0e2b7c7f0d1a6fcd0e71cb
Sidharth Varma <sidharthvarma@Sidharths-MacBook-Air.local>
2020-08-28 14:38:51 +1200

    Posterior needs testing lol

--- model_functions.py
+++ model_functions.py
@@ -1,3 +1,5 @@
+#import this file to any other file to access the functions
+
 import numpy as np
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
@@ -225,48 +227,3 @@ def LMP_Model(t, b ,b1, alpha, bc,tau):
     # INTERPOLATE to T (from Tcon)
     C_interp = np.interp(t,tv,C)
     return C_interp
-
-
-# Solve pressure ODE
-pi = 0
-t = np.arange(1980,2020,step = 0.5)
-#parameters
-b=0.5
-b1=0.5
-alpha=0
-bc=1
-tau = 5
-# LMP_Model(t,b,b1,alpha,bc,tau)
-
-
-# Testing curve_fit
-# load in cow data and concentration data
-tcon, c = np.genfromtxt('nl_n.csv', delimiter=',', skip_header=1).T
-
-pars = curve_fit(LMP_Model,tcon,c,[1,1,1,1,15])
-print(pars)
-b=pars[0][0]
-b1=pars[0][1]
-alpha=pars[0][2]
-bc=pars[0][3]
-tau = pars[0][4]
-C = LMP_Model(t,b,b1,alpha,bc,tau)
-tau_array = np.ones(t.shape)*(tau/2)
-
-f,ax = plt.subplots(1,1)
-
-ax.plot(t,C,'k', label = 'Numeric Solution')
-ax.plot(tcon,c,'r+', label = 'Data')
-ax.set_title('Numerical Solution and data')
-plt.ylabel('Concentration')
-plt.xlabel('Time')
-
-'''
-# Plotting cows for reference
-ax2 = ax.twinx()
-ax2.plot(tn,n,'b', label = 'Data cows')
-ax2.set_ylabel('Number of cows')
-ax.legend() 
-'''
-    
-plt.show()
