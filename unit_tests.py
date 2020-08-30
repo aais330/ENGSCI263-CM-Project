a138ae2bfe9e48f8ade67dfeff92c3c169e989db
Annie Li <annie@Annies-MacBook-Pro-3.local>
2020-08-30 14:50:25 +1200

    fixed convergence

--- unit_tests.py
+++ unit_tests.py
@@ -3,48 +3,30 @@ from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
 from model_functions import *
 
-def test_concmodel():
-    # Tests for concentration model
-    # dCdt(ci, t, P, b1, alpha, bc, tau)
-    # -ni*b1*(P-dP_surf)+bc*ci*(P-(dP_a/2))
-    # pars=[P[i],b1,alpha,bc,tau]
-    # improved_euler_step(f, tk, xk, h, pars = [])
-    b=0.5
-    b1=0.5
-    alpha=0
-    bc=1
-    tau = 5
-    dP_a = 0.1 
-    dP_surf = 0.05 
-    t_mar = 2020 
-    t_acs = 2010 
-    tol=0.1
-    # *need to put everything through IE calculator*
-    # Tests when t-tau < 1990.5
 
-    # Test when n=0
+# Tests for concentration model
+# dCdt(ci, t, P, b1, alpha, bc, tau)
+# -ni*b1*(P-dP_surf)+bc*ci*(P-(dP_a/2))
+# pars=[P[i],b1,alpha,bc,tau]
+# improved_euler_step(f, tk, xk, h, pars = [])
+b=0.5
+b1=0.5
+alpha=0
+bc=1
+tau = 5
+dP_a = 0.1 
+dP_surf = 0.05 
+t_mar = 2020 
+t_acs = 2010 
+tol=1.-10
 
-    # Tests when t-tau > 1990.5 
-    # Test when t-tau>t_acs 2015>2010
-    Test=improved_euler_step(dCdt,2020,0.2,1,pars=[0,b1,alpha,bc,tau])
-    # assert(abs(Test-0.1665)<tol)
-    print('Test 1 passed')
-    # Test when t-tau<t_acs 2009<2010
-    Test=improved_euler_step(dCdt,2014,0.2,1,pars=[0,b1,alpha,bc,tau])
-    # assert(abs(Test-13870.7)<tol)
-    print('Test 2 passed')
-    # Test when t>t_mar
-    Test=improved_euler_step(dCdt,2021,0.2,1,pars=[0,b1,alpha,bc,tau])
-
-    # Test when t<t_mar
-    Test=improved_euler_step(dCdt,2018,0.2,1,pars=[0,b1,alpha,bc,tau])
 def test_dPdt():
     #2 tests for dPdt
     check_dPdt = dPdt(10, 2015, 3)
-    assert((abs(check_dPdt-60))<1.-10)
+    assert((abs(check_dPdt-60))<tol)
 
     check_dPdt2 = dPdt(20, 2025, 6)
-    assert((abs(check_dPdt2)-238.5)<1.-10)
+    assert((abs(check_dPdt2)-238.5)<tol)
 
     print("dPdt passed")
 
@@ -52,10 +34,28 @@ def test_dCdt():
     #2 tests for dCdt
     
     check_dCdt = dCdt(20, 2005, 0.4, 1, 2, 1, 3) 
-    #assert((abs(check_dCdt-121818.25))<1.-10) # incorrect calculation
+    #assert((abs(check_dCdt-121818.25))<tol) # incorrect calculation
 
     check_dCdt2 = dCdt(25, 2016, 0.2, 1, 2, 1, 5) 
-    assert((abs(check_dCdt2)-212666)<1.-10) # this seems right
+    assert((abs(check_dCdt2)-212666)<tol) # this seems right
+
+    # Tests when t-tau > 1990.5 
+    # Test when t-tau>t_acs 2015>2010
+    check_dCdt3=improved_euler_step(dCdt,2020,0.2,1,pars=[0,b1,alpha,bc,tau])
+    # assert(abs(Test-0.1665)<tol)
+
+    # Test when t-tau<t_acs 2009<2010
+    check_dCdt4=improved_euler_step(dCdt,2014,0.2,1,pars=[0,b1,alpha,bc,tau])
+    # assert(abs(Test-13870.7)<tol)
+
+    # Test when t>t_mar
+    check_dCdt5=improved_euler_step(dCdt,2021,0.2,1,pars=[0,b1,alpha,bc,tau])  
+
+    # Test when t<t_mar
+    check_dCdt6=improved_euler_step(dCdt,2018,0.2,1,pars=[0,b1,alpha,bc,tau])
+
+    # Tests when t-tau < 1990.5
+
 
     print("dCdt passed")
 
