667e80cc6ccf9c05906759076b8b70dbd15b45bd
Anton Aish <antonaish@Antons-MacBook-Pro.local>
2020-09-11 11:11:42 +1200

    Organised files and main script template

---
+++ main.py
@@ -0,0 +1,35 @@
+###############################################################
+# This scipt generates all the related figures for the nitrate
+# leaching project
+##############################################################
+
+# Plotting the data
+
+
+# unit tests
+
+
+# Convergence Analysis
+
+
+
+# Benchmarks
+
+
+
+# Intial model
+
+
+
+# Improved model
+
+
+
+
+# Use forecasting and without active carbon sink
+
+
+
+
+# Forecasts with uncertainty
+
